a6c70d40788373570214d22797a69cb99ba44e90
RobertMcGregorIsCool <robert.mcgregor@gmail.com>
2024-11-14 18:14:10 +0000

    Added explanatory variables.

--- 16_SFML_Games/snake.cpp
+++ 16_SFML_Games/snake.cpp
@@ -2,10 +2,10 @@
 #include <time.h>
 using namespace sf;
 
-const int GRID_HOR = 30,GRID_VER= 20;
+const int GRID_HOR_MAX = 30,GRID_VER_MAX= 20;
 const int PIXELS_PER_SQUARE     = 16;
-const int WINDOW_WIDTH          = PIXELS_PER_SQUARE * GRID_HOR;
-const int WINDOW_HEIGHT         = PIXELS_PER_SQUARE * GRID_VER;
+const int WINDOW_WIDTH          = PIXELS_PER_SQUARE * GRID_HOR_MAX;
+const int WINDOW_HEIGHT         = PIXELS_PER_SQUARE * GRID_VER_MAX;
 
 int direction   = 0;
 const int SOUTH = 0;
@@ -48,19 +48,22 @@ void Tick()
         (snakeBody[0].segmentYpos == fruit.posY)) 
      {
         snakeLengthCurrent++; 
-        fruit.posX = rand()%GRID_HOR; 
-        fruit.posY = rand()%GRID_VER;
+        fruit.posX = rand()%GRID_HOR_MAX; 
+        fruit.posY = rand()%GRID_VER_MAX;
     }
 
-    if (snakeBody[snakeHead].segmentXpos>GRID_HOR)  snakeBody[snakeHead].segmentXpos = 0;
-    if (snakeBody[snakeHead].segmentXpos<0)         snakeBody[snakeHead].segmentXpos = GRID_HOR;
-    if (snakeBody[snakeHead].segmentYpos>GRID_VER)  snakeBody[snakeHead].segmentYpos = 0;
-    if (snakeBody[snakeHead].segmentYpos<0)         snakeBody[snakeHead].segmentYpos = GRID_VER;
+    if (snakeBody[snakeHead].segmentXpos > GRID_HOR_MAX)  snakeBody[snakeHead].segmentXpos = 0;
+    if (snakeBody[snakeHead].segmentXpos < 0)         snakeBody[snakeHead].segmentXpos = GRID_HOR_MAX;
+    if (snakeBody[snakeHead].segmentYpos > GRID_VER_MAX)  snakeBody[snakeHead].segmentYpos = 0;
+    if (snakeBody[snakeHead].segmentYpos < 0)         snakeBody[snakeHead].segmentYpos = GRID_VER_MAX;
  
     for (int i = 1; i < snakeLengthCurrent; i++)
     {
-        if (snakeBody[snakeHead].segmentXpos == snakeBody[i].segmentXpos &&
-            snakeBody[snakeHead].segmentYpos == snakeBody[i].segmentYpos)
+        bool snakeHeadOverlapsBodySegmentX = snakeBody[snakeHead].segmentXpos == snakeBody[i].segmentXpos;
+        
+        bool snakeHeadOVerlapsBodySegmentY = snakeBody[snakeHead].segmentYpos == snakeBody[i].segmentYpos;
+        
+        if (snakeHeadOverlapsBodySegmentX && snakeHeadOVerlapsBodySegmentY)            
         {
             snakeLengthCurrent = i;
         }
@@ -114,8 +117,8 @@ int snake()
    ////// draw  ///////
     window.clear();
 
-    for (int iX = 0; iX < GRID_HOR; iX++) 
-      for (int iY = 0; iY < GRID_VER; iY++) 
+    for (int iX = 0; iX < GRID_HOR_MAX; iX++) 
+      for (int iY = 0; iY < GRID_VER_MAX; iY++) 
         { 
           spriteBackground.setPosition(iX * PIXELS_PER_SQUARE, iY * PIXELS_PER_SQUARE);
           window.draw(spriteBackground);
@@ -123,11 +126,15 @@ int snake()
 
     for (int i=0;i<snakeLengthCurrent;i++)
         { 
-        spriteSnake.setPosition(snakeBody[i].segmentXpos * PIXELS_PER_SQUARE, snakeBody[i].segmentYpos * PIXELS_PER_SQUARE);
+        int snakeSegmentScreenXPos = snakeBody[i].segmentXpos * PIXELS_PER_SQUARE;
+        int snakeSegmentScreenYPos = snakeBody[i].segmentYpos * PIXELS_PER_SQUARE;
+        spriteSnake.setPosition(snakeSegmentScreenXPos,snakeSegmentScreenYPos);
         window.draw(spriteSnake); 
         }
    
-    spriteSnake.setPosition(fruit.posX * PIXELS_PER_SQUARE, fruit.posY * PIXELS_PER_SQUARE);  
+    int fruitScreenXPos = fruit.posX * PIXELS_PER_SQUARE;
+    int fruitScreenYPos = fruit.posY * PIXELS_PER_SQUARE;
+    spriteSnake.setPosition(fruitScreenXPos, fruitScreenYPos);  
     window.draw(spriteSnake);    
 
     window.display();
