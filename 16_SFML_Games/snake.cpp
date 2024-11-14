f3d2e438fe461ef2bd95cb2979755a9b2be89f21
RobertMcGregorIsCool <robert.mcgregor@gmail.com>
2024-11-14 17:55:11 +0000

    refactor: Removed magic numbers (literals), also reorganised some layout

--- 16_SFML_Games/snake.cpp
+++ 16_SFML_Games/snake.cpp
@@ -2,52 +2,67 @@
 #include <time.h>
 using namespace sf;
 
-const int GRID_HOR=30,GRID_VER=20;
-int pixelsPerSquare=16;
-int windowWidth = pixelsPerSquare*GRID_HOR;
-int windowHeight = pixelsPerSquare*GRID_VER;
+const int GRID_HOR = 30,GRID_VER= 20;
+const int PIXELS_PER_SQUARE     = 16;
+const int WINDOW_WIDTH          = PIXELS_PER_SQUARE * GRID_HOR;
+const int WINDOW_HEIGHT         = PIXELS_PER_SQUARE * GRID_VER;
 
-int direction;
+int direction   = 0;
+const int SOUTH = 0;
+const int WEST  = 1;
+const int EAST  = 2;
+const int NORTH = 3;
 
-int snakeLength =4;
+int snakeHead   = 0;
+int snakeLengthCurrent = 4;
+const int SNAKE_LENGTH_MAX = 100;
 
-struct Snake 
-{ int segmentXpos,segmentYpos;}  snakeBody[100];
+int fruitStartingPos = 10;
+
+struct Snake
+{
+    int segmentXpos = 0;
+    int segmentYpos = 0;
+}   snakeBody[SNAKE_LENGTH_MAX];
 
 struct Fruit
-{ int x,y;} fruit;
+{
+    int posX = 0;
+    int posY = 0;
+} fruit;
 
 void Tick()
  {
-    for (int i = snakeLength; i > 0; --i)
+    for (int i = snakeLengthCurrent; i > 0; --i)
     {  
-        snakeBody[i].segmentXpos = snakeBody[i-1].segmentXpos; 
+        snakeBody[i].segmentXpos = snakeBody[i-1].segmentXpos;
         snakeBody[i].segmentYpos = snakeBody[i-1].segmentYpos;
     }
 
-    if (direction==0) snakeBody[0].segmentYpos+=1;      
-    if (direction==1) snakeBody[0].segmentXpos-=1;        
-    if (direction==2) snakeBody[0].segmentXpos+=1;         
-    if (direction==3) snakeBody[0].segmentYpos-=1;   
+    if (direction == SOUTH) snakeBody[snakeHead].segmentYpos += 1;
+    if (direction == WEST)  snakeBody[snakeHead].segmentXpos -= 1;
+    if (direction == EAST)  snakeBody[snakeHead].segmentXpos += 1;
+    if (direction == NORTH) snakeBody[snakeHead].segmentYpos -= 1;
 
-    if ((snakeBody[0].segmentXpos==fruit.x) && (snakeBody[0].segmentYpos==fruit.y)) 
+    if ((snakeBody[0].segmentXpos == fruit.posX) && 
+        (snakeBody[0].segmentYpos == fruit.posY)) 
      {
-        snakeLength++; 
-        fruit.x=rand()%GRID_HOR; 
-        fruit.y=rand()%GRID_VER;
+        snakeLengthCurrent++; 
+        fruit.posX = rand()%GRID_HOR; 
+        fruit.posY = rand()%GRID_VER;
     }
 
-    if (snakeBody[0].segmentXpos>GRID_HOR)  snakeBody[0].segmentXpos=0;  
-    if (snakeBody[0].segmentXpos<0)         snakeBody[0].segmentXpos=GRID_HOR;
-    if (snakeBody[0].segmentYpos>GRID_VER)  snakeBody[0].segmentYpos=0;  
-    if (snakeBody[0].segmentYpos<0)         snakeBody[0].segmentYpos=GRID_VER;
+    if (snakeBody[snakeHead].segmentXpos>GRID_HOR)  snakeBody[snakeHead].segmentXpos = 0;
+    if (snakeBody[snakeHead].segmentXpos<0)         snakeBody[snakeHead].segmentXpos = GRID_HOR;
+    if (snakeBody[snakeHead].segmentYpos>GRID_VER)  snakeBody[snakeHead].segmentYpos = 0;
+    if (snakeBody[snakeHead].segmentYpos<0)         snakeBody[snakeHead].segmentYpos = GRID_VER;
  
-    for (int i = 1; i < snakeLength; i++)
+    for (int i = 1; i < snakeLengthCurrent; i++)
     {
-        if (snakeBody[0].segmentXpos == snakeBody[i].segmentXpos && 
-            snakeBody[0].segmentYpos == snakeBody[i].segmentYpos)  
+        if (snakeBody[snakeHead].segmentXpos == snakeBody[i].segmentXpos &&
+            snakeBody[snakeHead].segmentYpos == snakeBody[i].segmentYpos)
         {
-            snakeLength = i;
+            snakeLengthCurrent = i;
         }
     }
  }
@@ -56,7 +71,7 @@ int snake()
 {  
     srand(time(0));
 
-    RenderWindow window(VideoMode(windowWidth, windowHeight), "Snake Game!");
+    RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Snake Game!");
 
     Texture t1White,t2Red;
     t1White.loadFromFile("images/snake/white.png");
@@ -66,10 +81,11 @@ int snake()
     Sprite spriteSnake(t2Red);
 
     Clock clock;
-    float tickTimer=0, tickDelay=0.1;
+    float tickTimer = 0;
+    float tickDelay = 0.1f;
 
-    fruit.x=10;
-    fruit.y=10; 
+    fruit.posX = fruitStartingPos;
+    fruit.posY = fruitStartingPos; 
     
     while (window.isOpen())
     {
@@ -77,45 +93,44 @@ int snake()
         clock.restart();
         tickTimer += elapsedTime; 
 
-        Event e;
-        while (window.pollEvent(e))
+        Event eventCurrent;
+        while (window.pollEvent(eventCurrent))
         {
-            if (e.type == Event::Closed)      
+            if (eventCurrent.type == Event::Closed)      
                 window.close();
         }
 
-        if (Keyboard::isKeyPressed(Keyboard::Left)) direction=1;   
-        if (Keyboard::isKeyPressed(Keyboard::Right)) direction=2;    
-        if (Keyboard::isKeyPressed(Keyboard::Up)) direction=3;
-        if (Keyboard::isKeyPressed(Keyboard::Down)) direction=0;
+        if (Keyboard::isKeyPressed(Keyboard::Left))     direction = WEST;   
+        if (Keyboard::isKeyPressed(Keyboard::Right))    direction = EAST;    
+        if (Keyboard::isKeyPressed(Keyboard::Up))       direction = NORTH;
+        if (Keyboard::isKeyPressed(Keyboard::Down))     direction = SOUTH;
 
-        if (tickTimer>tickDelay) 
+        if (tickTimer > tickDelay) 
         {
-            tickTimer=0; 
+            tickTimer = 0; 
             Tick();
         }
 
    ////// draw  ///////
     window.clear();
 
-    for (int i=0; i<GRID_HOR; i++) 
-      for (int j=0; j<GRID_VER; j++) 
+    for (int iX = 0; iX < GRID_HOR; iX++) 
+      for (int iY = 0; iY < GRID_VER; iY++) 
         { 
-          spriteBackground.setPosition(i * pixelsPerSquare, j * pixelsPerSquare);  
-          window.draw(spriteBackground); 
+          spriteBackground.setPosition(iX * PIXELS_PER_SQUARE, iY * PIXELS_PER_SQUARE);
+          window.draw(spriteBackground);
         }
 
-    for (int i=0;i<snakeLength;i++)
+    for (int i=0;i<snakeLengthCurrent;i++)
         { 
-        spriteSnake.setPosition(snakeBody[i].segmentXpos * pixelsPerSquare, snakeBody[i].segmentYpos * pixelsPerSquare);
+        spriteSnake.setPosition(snakeBody[i].segmentXpos * PIXELS_PER_SQUARE, snakeBody[i].segmentYpos * PIXELS_PER_SQUARE);
         window.draw(spriteSnake); 
         }
    
-    spriteSnake.setPosition(fruit.x * pixelsPerSquare, fruit.y * pixelsPerSquare);  
+    spriteSnake.setPosition(fruit.posX * PIXELS_PER_SQUARE, fruit.posY * PIXELS_PER_SQUARE);  
     window.draw(spriteSnake);    
 
     window.display();
     }
-
     return 0;
 }
